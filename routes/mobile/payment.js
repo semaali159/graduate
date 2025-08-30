6fd91adf2c1ad560960bd9b453e4c3566d5b91e8
semaali159 <semaali1599@gmail.com>
2025-08-30 18:25:16 +0300

    update index after payment3

--- routes/mobile/payment.js
+++ routes/mobile/payment.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createPaymentIntent } = require("../../controllers/mobile/payment");
+const { createPaymentIntent, s } = require("../../controllers/mobile/payment");
 const { verifyToken } = require("../../middlewares/verifyToken");
 const router = express.Router();
 
