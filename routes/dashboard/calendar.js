68a0788359a379f369423c998ff5c83bfbbcbdd6
semaali159 <semaali1599@gmail.com>
2025-09-06 19:48:50 +0300

    add verification calendar for admin

--- routes/dashboard/calendar.js
+++ routes/dashboard/calendar.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { getEventForCalendar } = require("../../controllers/dashboard/calendar");
+const { verifyTokenAndAdmin } = require("../../middlewares/verifyToken");
 const router = express.Router();
-router.get("/", getEventForCalendar);
+router.get("/", verifyTokenAndAdmin, getEventForCalendar);
 module.exports = router;
