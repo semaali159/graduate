94ff5ee8811af716eb61e44872b499501b726aee
semaali159 <semaali1599@gmail.com>
2025-09-16 00:20:46 +0300

    add read notification

--- controllers/mobile/notification.js
+++ controllers/mobile/notification.js
@@ -10,4 +10,18 @@ const getAllUserNotification = asyncHandler(async (req, res) => {
   }
   return res.status(200).json({ message: "notifications", notifications });
 });
-module.exports = { getAllUserNotification };
+const readNotification = asyncHandler(async (req, res) => {
+  const notificationId = req.params.id;
+  // const notification = await db.notification.findOne(notificationId);
+  const [updatedRows] = await db.notification.update(
+    { isRead: true },
+    { where: { id: notificationId } }
+  );
+
+  if (updatedRows === 0) {
+    return res.status(404).json({ message: "Notification not found" });
+  }
+
+  return res.status(200).json({ message: "Notification marked as read" });
+});
+module.exports = { getAllUserNotification, readNotification };
--- routes/mobile/notification.js
+++ routes/mobile/notification.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const {
   getAllUserNotification,
+  readNotification,
 } = require("../../controllers/mobile/notification");
-const { verifyTokenAndUser } = require("../../middlewares/verifyToken");
+const {
+  verifyTokenAndUser,
+  verifyToken,
+} = require("../../middlewares/verifyToken");
 const router = express.Router();
 router.get("/all/:id", verifyTokenAndUser, getAllUserNotification);
+router.put("/read/:id", verifyToken, readNotification);
 module.exports = router;
